d7a6a5b6fd65d18c96b4c7d718cb94e53b1c541c
AInan05 <147404730+AInan05@users.noreply.github.com>
2023-10-27 17:03:38 +0100

    Update BareBonesInterpreter.java

--- BareBonesInterpreter.java
+++ BareBonesInterpreter.java
@@ -21,6 +21,7 @@ public class BareBonesInterpreter {
     List<String> code = new ArrayList<>();
     Integer lineNo = 0;
     Stack<Integer> whileStack = new Stack<>();
+    Stack<Subroutine> callStack = new Stack<>();
     String operations = "(incr|decr|clear)";
     String operationRegex = operations + "\\s([a-z]|[A-Z])+;";
     String iterations = "while";
@@ -32,8 +33,12 @@ public class BareBonesInterpreter {
     I HATE REGEX I HATE REGEX I HATE REGEX I HATE REGEX I HATE REGEX I HATE REGEX I HATE REGEX*/
     String arithmetics = "[+\\-/*]";
     String arithmeticsRegex = "([a-z]|[A-Z])+(\\s)*=(\\s)*(([a-z]|[A-Z])+|(\\d)+)(\\s)*"+ arithmetics +"(\\s)*(([a-z]|[A-Z])+|(\\d)+);";
+    String assignmentRegex = "let\\s([a-z]|[A-Z])+\\s=\\s((\\d)+|(([a-z]|[A-Z])+[(](\\d+,)*\\d?[)]));";
+    String subDefRegex = "def([a-z]|[A-Z])+[(](\\d+,)*\\d?[)]";
+    String returnRegex = "return\\s([a-z]|[A-Z])+;";
     boolean skip = false;
-    boolean endFound = false;
+    Dictionary<String, String[]> subroutineDict = new Hashtable<>(); //maps identifiers to subroutine data
+
 
      //Open file method that checks for the correct file type and if the file exists.
      //If conditions are satisfied open the file.
@@ -59,22 +64,12 @@ public class BareBonesInterpreter {
         // While program has lines to interpret.
         while (lineNo < code.size()) {
             // Get line.
-            String s = code.get(lineNo);
+            String line = code.get(lineNo);
+            String s = line.trim();
             String errorCheck = "pass";
-            // Check for end statements.
-            if (indentation >= 3) {
-                if (s.substring(indentation - 3).startsWith("end;")) {
-                    // Decrease the indentation level.
-                    indentation -= 3;
-                    endFound = true;
-                }
-            }
-            // Remove whitespaces.
-            s = s.substring(indentation);
-            // If skip mode is inactivated or an end statement is encountered, execute the code.
-            if (!skip || endFound) {
-                errorCheck = execute(s, endFound);
-            }
+            // Count indentation
+            int currentIndentation = line.indexOf(s.substring(0,1));
+            errorCheck = execute(s, currentIndentation);
             // Return if an error was encountered.
             if (!errorCheck.equals("pass")) {
                 return errorCheck;
@@ -85,49 +80,121 @@ public class BareBonesInterpreter {
         return "pass";
     }
 
-    public String execute(String s, boolean endFound) {
-        System.out.println(s);
+    public String execute(String s, int currentIndentation) {
         String errorCheck = "pass";
         // Run error check.
-        String syntaxCheck = syntaxErrorCheck(s); //this method is a general syntax check now tho
-        // Return if an error was encountered.
-        if (syntaxCheck.equals("Syntax Error")) {
-            return syntaxCheck;
+        if (s.equals("end;") && currentIndentation == indentation - 3) {
+            System.out.println(s);
+            indentation -= 3;
+            end();
+        } else if (s.equals("endSub;") && currentIndentation == indentation - 3) {
+            System.out.println(s);
+            indentation -= 3;
+            endSubroutine();
         }
-        // Call methods.
-        switch(syntaxCheck) {
-            case "operations":
-                errorCheck = executeOperation(s);
-                break;
-            case "arithmetics":
-                errorCheck = executeArithmetic(s);
-                break;
-            case "iterations":
-                executeIteration(s);
-                break;
-            case "end":
-                if (!endFound){
-                    return "Unexpected end";
-                }
-                end();
-                break;
+        else if (!skip) {
+            System.out.println(s);
+            String syntaxCheck = syntaxErrorCheck(s, currentIndentation); //this method is a general syntax check now
+            // Return if an error was encountered.
+            if (syntaxCheck.equals("Syntax Error")) {
+                return syntaxCheck;
+            }
+            // Call methods.
+            switch (syntaxCheck) {
+                case "operations":
+                    errorCheck = executeOperation(s);
+                    break;
+                case "arithmetics":
+                    errorCheck = executeArithmetic(s);
+                    break;
+                case "iterations":
+                    executeIteration(s);
+                    errorCheck = "pass";
+                    break;
+                case "assignment":
+                    errorCheck = assign(s);
+                    break;
+                case "defSub":
+                    defineSub(s);
+                    errorCheck = "pass";
+                    break;
+                case "return":
+                    returnSubroutine(s);
+            }
         }
         return errorCheck;
     }
 
-    public String syntaxErrorCheck(String line) {
+    public String syntaxErrorCheck(String line, int currentIndentation) {
         // Check if line matches regex.
-        if (line.matches(operationRegex)) {
+        if (line.matches(operationRegex) && currentIndentation == indentation) {
             return "operations";
-        } else if (line.matches(arithmeticsRegex)) {
+        } else if (line.matches(arithmeticsRegex) && currentIndentation == indentation) {
             return "arithmetics";
-        } else if (line.matches(iterationRegex)) {
+        } else if (line.matches(iterationRegex) && currentIndentation == indentation) {
             return "iterations";
-        } else if (line.matches("end;")) {
-            return "end";
+        } else if (line.matches(assignmentRegex) && currentIndentation == indentation) {
+            return "assignment";
+        } else if (line.matches(subDefRegex) && currentIndentation == indentation) {
+            return "defSub";
+        } else if (line.matches(returnRegex) && currentIndentation == indentation) {
+            return "return";
         }
         // Unknown keyword.
         return "Syntax Error";
+    }
+
+    public String assign(String s) {
+        String[] stuff = s.split("[\\s=]+|;");
+        String varName = stuff[1];
+        Integer value = Integer.valueOf(stuff[2]);
+        if (stuff[2].matches("([a-z]|[A-Z])+[(].+")) {
+            callSubroutine(s.substring(s.indexOf(stuff[2])));
+        }
+        if (variables.put(varName, value) == null) {
+            return "Var not found";
+        }
+        System.out.println(variables);
+        return "pass";
+    }
+
+    public void defineSub(String s) { //def subName(parameters);
+        // Divide the line into parts.
+        String[] stuff = s.split("[\\s;()]");
+        String identifier = stuff[0];
+        String[] subData = new String[stuff.length - 3];
+        subData[0] = String.valueOf(lineNo);
+        System.arraycopy(stuff, 2, subData, 1, subData.length - 1);
+        subroutineDict.put(identifier, subData);
+    }
+
+    public void callSubroutine(String s) {
+        // Divide the line into parts.
+        String[] stuff = s.split("[\\s;()]");
+        String identifier = stuff[0];
+        Integer[] arguments = new Integer[stuff.length - 2];
+        for (int i = 0 ; i < arguments.length ; i++) {
+            arguments[i] = Integer.valueOf(stuff[i + 1]);
+        }
+        Subroutine newSub = new Subroutine(lineNo, subroutineDict.get(identifier), arguments);
+        this.callStack.push(newSub);
+        lineNo = Integer.valueOf(subroutineDict.get(identifier)[0]);
+        indentation += 3;
+    }
+
+    public void endSubroutine() {
+        lineNo = callStack.pop().getReturnLineNo();
+        indentation -= 3;
+    }
+
+    public void returnSubroutine(String varName) {
+        Subroutine sub = callStack.pop();
+        lineNo = sub.getReturnLineNo();
+        Integer value = sub.getLocalVariables(varName);
+        String line = code.get(lineNo);
+        String s = line.trim();
+        String[] stuff = s.split("[\\s=]+;");
+        variables.put(stuff[0], value);
     }
 
     public String executeOperation(String s) {
@@ -160,16 +227,24 @@ public class BareBonesInterpreter {
         String var1 = stuff[0];
         Integer var2;
         Integer var3;
-        Integer result;
+        int result;
         if (stuff[2].matches("\\d+")) {
             var2 = Integer.valueOf(stuff[2]);
         } else {
-            var2 = variables.get(stuff[2]);
+            if (variables.get(stuff[2]) != null){
+                var2 = variables.get(stuff[2]);
+            } else {
+                return "Var not found";
+            }
         }
         if (stuff[4].matches("\\d+")) {
             var3 = Integer.valueOf(stuff[4]);
         } else {
-            var3 = variables.get(stuff[4]);
+            if (variables.get(stuff[4]) != null){
+                var3 = variables.get(stuff[4]);
+            } else {
+                return "Var not found";
+            }
         }
         switch(keyword) {
             case "+":
@@ -256,7 +331,6 @@ public class BareBonesInterpreter {
     }
 
     public void end() {
-        endFound = false;
         if (skip) {
             // Set skip to false and remove the last while from while stack.
             skip = false;
